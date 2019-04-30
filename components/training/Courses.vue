0ee79e3350255c17f0d2c1ae99bf1d5a657e0d4d
Hunter Nunnery <hunter.nunnery@yahoo.com>
2019-04-29 19:38:13 -0500

    Update Courses.vue

--- components/training/Courses.vue
+++ components/training/Courses.vue
@@ -2,13 +2,14 @@
   <v-container fluid class="pb-5 orange">
     <v-layout row wrap justify-center align-center class="pa-0 ma-0">
       <v-flex xs12 xl10>
+        <h2 class="display-1 secondary--text text-xs-center my-3">Choose a Course</h2>
         <v-layout row wrap justify-left>
           <v-flex xs12 sm6 md3 xl2 class="text-xs-center pa-3">
             <nuxt-link to="/courses/ceh">
               <v-img
                 src="https://res.cloudinary.com/missionwebdev/image/upload/f_auto/v1551134832/StealthEntry/eccouncil/CEH-Cover-v10-2.jpg"
                 alt="CEH"
-                class="responsive-img elevation-12"
+                class="responsive-img elevation-12 course-tile"
               ></v-img>
             </nuxt-link>
           </v-flex>
@@ -17,7 +18,7 @@
               <v-img
                 src="https://res.cloudinary.com/missionwebdev/image/upload/f_auto/v1551134832/StealthEntry/eccouncil/CND-cover-main.jpg"
                 alt="CND"
-                class="responsive-img elevation-12"
+                class="responsive-img elevation-12 course-tile"
               ></v-img>
             </nuxt-link>
           </v-flex>
@@ -26,7 +27,7 @@
               <v-img
                 src="https://res.cloudinary.com/missionwebdev/image/upload/f_auto/v1556397355/StealthEntry/eccouncil/CHFI-cover-main.jpg"
                 alt="CHFI"
-                class="responsive-img elevation-12"
+                class="responsive-img elevation-12 course-tile"
               ></v-img>
             </nuxt-link>
           </v-flex>
@@ -35,7 +36,7 @@
               <v-img
                 src="https://res.cloudinary.com/missionwebdev/image/upload/f_auto/v1551134832/StealthEntry/eccouncil/ECSAV10-Cover.jpg"
                 alt="ECSA"
-                class="responsive-img elevation-12"
+                class="responsive-img elevation-12 course-tile"
               ></v-img>
             </nuxt-link>
           </v-flex>
@@ -44,7 +45,7 @@
               <v-img
                 src="https://res.cloudinary.com/missionwebdev/image/upload/v1556397505/StealthEntry/eccouncil/CCISO-cover-main.jpg"
                 alt="CCISO"
-                class="responsive-img elevation-12"
+                class="responsive-img elevation-12 course-tile"
               ></v-img>
             </nuxt-link>
           </v-flex>
@@ -53,7 +54,7 @@
               <v-img
                 src="https://res.cloudinary.com/missionwebdev/image/upload/f_auto/v1556397512/StealthEntry/eccouncil/CSCU-cover-main.jpg"
                 alt="CSCU"
-                class="responsive-img elevation-12"
+                class="responsive-img elevation-12 course-tile"
               ></v-img>
             </nuxt-link>
           </v-flex>
@@ -62,7 +63,7 @@
               <v-img
                 src="https://res.cloudinary.com/missionwebdev/image/upload/f_auto/v1556397749/StealthEntry/eccouncil/EDRP-featured.jpg"
                 alt="EDRP"
-                class="responsive-img elevation-12"
+                class="responsive-img elevation-12 course-tile"
               ></v-img>
             </nuxt-link>
           </v-flex>
@@ -71,7 +72,7 @@
               <v-img
                 src="https://res.cloudinary.com/missionwebdev/image/upload/f_auto/v1556397883/StealthEntry/eccouncil/ECIH-Product-Cover.jpg"
                 alt="ECIH"
-                class="responsive-img elevation-12"
+                class="responsive-img elevation-12 course-tile"
               ></v-img>
             </nuxt-link>
           </v-flex>
@@ -81,5 +82,10 @@
   </v-container>
 </template>
 
+<style lang="scss" scoped>
+.course-tile:hover {
+  outline: 1px solid #ffffff;
+}
+</style>
 
 
